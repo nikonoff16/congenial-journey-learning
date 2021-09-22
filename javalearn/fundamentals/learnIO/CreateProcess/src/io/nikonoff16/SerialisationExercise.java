26f4dbb9ab12dd472104de536bbbab6bce6f905a
Victor Osipov <i.nikonoff16@gmail.com>
2021-09-22 12:22:49 +0500

    Done 5-th part of the course

--- javalearn/fundamentals/learnIO/CreateProcess/src/io/nikonoff16/SerialisationExercise.java
+++ javalearn/fundamentals/learnIO/CreateProcess/src/io/nikonoff16/SerialisationExercise.java
@@ -1,29 +1,40 @@
 package io.nikonoff16;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 import java.util.Objects;
 
 public class SerialisationExercise {
     public static void main(String[] args) throws IOException {
-        System.out.println("Test");
-        byte[] data = new byte[1000];
-        Animal[] deserialisedAnimals = deserializeAnimalArray(data);
-    }
-
-    public static Animal[] deserializeAnimalArray(byte[] data) {
-        Animal[] result;
-
-        try (ObjectInputStream outputStream = new ObjectInputStream()) {
-            outputStream.read(data);
-        } catch (IOException e) {
-            e.printStackTrace();
+        Animal[] animalM1 = {new Animal("Cat"), new Animal("Dog"), new Animal("Elephant"),
+                new Animal("Cock"), new Animal("Bull"), new Animal("Ant"),
+                new Animal("Tentecles"), new Animal("Worm")};
+        ByteArrayOutputStream bai = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bai);
+        oos.writeInt(animalM1.length);
+        for (int i = 0; i < animalM1.length; i++) {
+            oos.writeObject(animalM1[i]);
         }
+        oos.flush();
+        oos.close();
+        Animal[] animalM2 = deserializeAnimalArray(bai.toByteArray());
     }
 
-    private static class Animal implements Serializable {
+    public static Animal[] deserializeAnimalArray(byte[] data) throws IOException {
+        Animal[] result;
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(data))) {
+            int size = objectInputStream.readInt();
+            result = new Animal[size];
+
+            for (int i = 0; i < size; i++) {
+                result[i] = (Animal) objectInputStream.readObject();
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+        return result;
+    }
+
+    public static class Animal implements Serializable {
         private final String name;
 
         public Animal(String name) {
